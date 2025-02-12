982cd2ecbfdd1f363913caa3dc9e7b98c146530e
X-Shots <hyrum6566@gmail.com>
2025-02-12 12:28:30 -0700

    some classes

---
+++ Recipe.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+public class Recipe {
+    private String name;
+    private String description;
+    private int servings;
+    private List<Ingredient> ingredients;
+    private List<String> steps;
+    private int UserID;
+    private int recipeID;
+    
+
+}
